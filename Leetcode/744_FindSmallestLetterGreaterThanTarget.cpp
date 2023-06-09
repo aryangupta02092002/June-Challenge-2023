174cf815e99619be04a6097626b23aa77bcc6bbd
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-06-09 11:50:50 +0530

    June 9 - Strings | Binary Search

---
+++ Leetcode/744_FindSmallestLetterGreaterThanTarget.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int low = 0, high = letters.size()-1;
+        if(letters[high] <= target){
+            return letters[0];
+        }
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(letters[mid] > target){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return letters[low];
+    }
+};
